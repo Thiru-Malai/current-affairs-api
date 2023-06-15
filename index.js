78f080beb4e22b46320c17f5d559c09ade087825
Thirumalai K.G <malaithiru370@gmail.com>
2023-06-15 20:31:20 +0530

    today-current-affair

--- index.js
+++ index.js
@@ -1,47 +1,137 @@
-const express = require("express")
-const cherrio = require("cheerio")
-const axios = require("axios")
-const app = express()
+const express = require("express");
+const cherrio = require("cheerio");
+const axios = require("axios");
+const app = express();
 
-app.listen("8000", ()=>{
-    console.log("App Listenin To Port 8000")
-})
+app.listen("8000", () => {
+  console.log("App Listenin To Port 8000");
+});
 
-app.get("/", (req, res)=>{
-    res.json("Welcome To Current Affairs API")   
-})
+app.get("/", (req, res) => {
+  res.json("Welcome To Current Affairs API");
+});
 
-app.get("/news", (req, res)=>{
-    axios.get("https://byjusexamprep.com/daily-gk-update-15th-June-2023-i")
-    .then((response) =>{
-        const html = response.data
-        // console.log(html)
-        const $ = cherrio.load(html)
-        $('h3', html).each(function(){
-            const title = $(this).text()
-            console.log(title)
-        })
-        $('.content-container').each(function(){
-            const desc = $(this).text()
-            console.log(desc)
-        })
-    })
-})
+function isNumber(c) {
+  switch (c) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      return true;
+  }
+  return false;
+}
 
+app.get("/today", (req, res) => {
+  var today = new Date();
+  var month = today.toLocaleString("default", { month: "long" });
+  var year = today.getFullYear();
+  let day = today.getDate();
 
+  let dayString = day.toString()
 
-app.get("/today-quiz", (req, res)=>{
-    let todayques = []
-    axios.get("https://byjusexamprep.com/current-affairs")
-    .then((response)=>{
-        const html = response.data
-        const $ = cherrio.load(html)
-        $("li", "ol").each(function(){
-            const text = $(this).text()
-            todayques.push({
-                question: text
-            })
-        })
-        res.json(todayques)
-    })
-})
+  if ((dayString).length == 1) {
+    dayString = "0" + dayString;
+  }
+
+  axios
+    .get(
+      "https://www.bankersadda.com/daily-current-affairs-and-news-headlines-of-" +
+        day +
+        "th-" +
+        month +
+        "-" +
+        year +
+        "/"
+    )
+    .then((response) => {
+      const html = response.data;
+      const $ = cherrio.load(html);
+      let titles = [];
+      let description = [];
+      let currentaffairs = [];
+      $(".entry-content p strong", html).each(function () {
+        const title = $(this).text();
+        if (isNumber(title.charAt(0)) && title.includes(".")) {
+          titles.push(title);
+        }
+      });
+
+      $(".entry-content ul", html).each(function () {
+        const desc = $(this).text();
+        if (!desc.startsWith("Top 15") && !desc.startsWith("Check More")) {
+          description.push(desc);
+        }
+      });
+
+      for (let i = 0; i < titles.length; i++) {
+        currentaffairs.push({
+          title: titles[i],
+          description: description[i],
+        });
+      }
+
+      res.json(currentaffairs);
+    
+    });
+});
+
+// app.get("/today", (req, res) => {
+
+//   axios
+//     .get(
+//       "https://www.bankersadda.com/daily-current-affairs-and-news-headlines-of-15th-june-2023/"
+//     )
+//     .then((response) => {
+//       const html = response.data;
+//       const $ = cherrio.load(html);
+
+//       let titles = [];
+//       let description = [];
+//       let currentaffairs = [];
+      
+//       $(".entry-content p strong", html).each(function () {
+//         const title = $(this).text();
+//         if (isNumber(title.charAt(0)) && title.includes(".")) {
+//           titles.push(title);
+//         }
+//       });
+      
+//       $(".entry-content ul", html).each(function () {
+//         const desc = $(this).text();
+//         if (!desc.startsWith("Top 15") && !desc.startsWith("Check More")) {
+//           description.push(desc);
+//         }
+//       });
+//       for (let i = 0; i < titles.length; i++) {
+//         currentaffairs.push({
+//           title: titles[i],
+//           description: description[i],
+//         });
+//       }
+//       res.json(currentaffairs);
+//     });
+// });
+
+app.get("/today-quiz", (req, res) => {
+  let todayques = [];
+  let i = 1;
+  axios.get("https://byjusexamprep.com/current-affairs").then((response) => {
+    const html = response.data;
+    const $ = cherrio.load(html);
+    $("li", "ol").each(function () {
+      const text = $(this).text();
+      // let number = "question " + i
+      todayques.push({
+        question: text,
+      });
+    });
+    res.json(todayques);
+  });
+});
+
